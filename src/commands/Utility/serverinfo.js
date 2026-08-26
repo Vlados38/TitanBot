a438beba0e5fdb4f024674e30c49b844aed3544a
Vlados38 <54667505+Vlados38@users.noreply.github.com>
2026-08-26 18:32:59 +0300

    Update serverinfo.js

--- src/commands/Utility/serverinfo.js
+++ src/commands/Utility/serverinfo.js
@@ -5,54 +5,58 @@ import { InteractionHelper } from '../../utils/interactionHelper.js';
 
 export default {
     data: new SlashCommandBuilder()
-    .setName("serverinfo")
-    .setDescription("Get detailed information about the server"),
+        .setName("serverinfo")
+        .setDescription("Получить подробную информацию о сервере"),
 
-  async execute(interaction) {
-    const deferSuccess = await InteractionHelper.safeDefer(interaction);
-    if (!deferSuccess) {
-      logger.warn(`ServerInfo interaction defer failed`, {
-        userId: interaction.user.id,
-        guildId: interaction.guildId,
-        commandName: 'serverinfo'
-      });
-      return;
-    }
+    async execute(interaction) {
+        const deferSuccess = await InteractionHelper.safeDefer(interaction);
+        if (!deferSuccess) {
+            logger.warn(`Не удалось отложить взаимодействие ServerInfo`, {
+                userId: interaction.user.id,
+                guildId: interaction.guildId,
+                commandName: 'serverinfo'
+            });
+            return;
+        }
 
-    const guild = interaction.guild;
-    const owner = await guild.fetchOwner();
+        const guild = interaction.guild;
+        const owner = await guild.fetchOwner();
 
-    const createdTimestamp = Math.floor(guild.createdAt.getTime() / 1000);
+        const createdTimestamp = Math.floor(guild.createdAt.getTime() / 1000);
 
-    const embed = createEmbed({ title: `Server Info: ${guild.name}`, description: `Server ID: ${guild.id}` })
-      .setThumbnail(guild.iconURL({ size: 256 }))
-      .addFields(
-        { name: "Owner", value: owner.user.tag, inline: true },
-        { name: "Members", value: `${guild.memberCount}`, inline: true },
-        {
-          name: "Channels",
-          value: `${guild.channels.cache.size}`,
-          inline: true,
-        },
-        { name: "Roles", value: `${guild.roles.cache.size}`, inline: true },
-        {
-          name: "Boosts",
-          value: `Level ${guild.premiumTier} (${guild.premiumSubscriptionCount})`,
-          inline: true,
-        },
-        {
-          name: "Creation Date",
-          value: `<t:${createdTimestamp}:R>`,
-          inline: true,
-        },
-      );
+        const embed = createEmbed({
+            title: `Информация о сервере: ${guild.name}`,
+            description: `ID сервера: ${guild.id}`
+        })
+            .setThumbnail(guild.iconURL({ size: 256 }))
+            .addFields(
+                { name: "Владелец", value: owner.user.tag, inline: true },
+                { name: "Участники", value: `${guild.memberCount}`, inline: true },
+                {
+                    name: "Каналы",
+                    value: `${guild.channels.cache.size}`,
+                    inline: true,
+                },
+                { name: "Роли", value: `${guild.roles.cache.size}`, inline: true },
+                {
+                    name: "Бусты",
+                    value: `Уровень ${guild.premiumTier} (${guild.premiumSubscriptionCount})`,
+                    inline: true,
+                },
+                {
+                    name: "Дата создания",
+                    value: `<t:${createdTimestamp}:R>`,
+                    inline: true,
+                },
+            );
 
-    await InteractionHelper.safeEditReply(interaction, { embeds: [embed] });
-    logger.info(`ServerInfo command executed`, {
-      userId: interaction.user.id,
-      guildId: guild.id,
-      guildName: guild.name,
-      memberCount: guild.memberCount
-    });
-  },
+        await InteractionHelper.safeEditReply(interaction, { embeds: [embed] });
+
+        logger.info(`Команда ServerInfo выполнена`, {
+            userId: interaction.user.id,
+            guildId: guild.id,
+            guildName: guild.name,
+            memberCount: guild.memberCount
+        });
+    },
 };
