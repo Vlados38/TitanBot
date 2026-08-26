9b4c03795b77fefe352c96788c1a67d438d10efb
Vlados38 <54667505+Vlados38@users.noreply.github.com>
2026-08-26 18:00:53 +0300

    Update leveladd.js

--- src/commands/Leveling/leveladd.js
+++ src/commands/Leveling/leveladd.js
@@ -6,25 +6,27 @@ import { addLevels, getLevelingConfig } from '../../services/leveling/leveling.j
 import { createEmbed } from '../../utils/embeds.js';
 
 import { InteractionHelper } from '../../utils/interactionHelper.js';
+
 export default {
   data: new SlashCommandBuilder()
     .setName('leveladd')
-    .setDescription('Add levels to a user')
+    .setDescription('Добавить уровни пользователю')
     .addUserOption((option) =>
       option
         .setName('user')
-        .setDescription('The user to add levels to')
+        .setDescription('Пользователь, которому нужно добавить уровни')
         .setRequired(true)
     )
     .addIntegerOption((option) =>
       option
         .setName('levels')
-        .setDescription('Number of levels to add')
+        .setDescription('Количество уровней для добавления')
         .setRequired(true)
         .setMinValue(1)
     )
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .setDMPermission(false),
+
   category: 'Leveling',
 
   async execute(interaction, config, client) {
@@ -33,42 +35,60 @@ export default {
     const hasPermission = await checkUserPermissions(
       interaction,
       PermissionFlagsBits.ManageGuild,
-      'You need ManageGuild permission to use this command.'
+      'Для использования этой команды необходимо право **Управление сервером**.'
     );
+
     if (!hasPermission) return;
 
-    const levelingConfig = await getLevelingConfig(client, interaction.guildId);
+    const levelingConfig = await getLevelingConfig(
+      client,
+      interaction.guildId
+    );
+
     if (!levelingConfig?.enabled) {
       await InteractionHelper.safeEditReply(interaction, {
         embeds: [
           new EmbedBuilder()
             .setColor('#f1c40f')
-            .setDescription('The leveling system is currently disabled on this server.')
+            .setDescription(
+              'Система уровней в данный момент отключена на этом сервере.'
+            )
         ],
         flags: MessageFlags.Ephemeral
       });
+
       return;
     }
 
     const targetUser = interaction.options.getUser('user');
     const levelsToAdd = interaction.options.getInteger('levels');
 
-    const member = await interaction.guild.members.fetch(targetUser.id).catch(() => null);
+    const member = await interaction.guild.members
+      .fetch(targetUser.id)
+      .catch(() => null);
+
     if (!member) {
       throw new TitanBotError(
         `User ${targetUser.id} not found in this guild`,
         ErrorTypes.USER_INPUT,
-        'The specified user is not in this server.'
+        'Указанный пользователь не находится на этом сервере.'
       );
     }
 
-    const userData = await addLevels(client, interaction.guildId, targetUser.id, levelsToAdd);
+    const userData = await addLevels(
+      client,
+      interaction.guildId,
+      targetUser.id,
+      levelsToAdd
+    );
 
     await InteractionHelper.safeEditReply(interaction, {
       embeds: [
         createEmbed({
-          title: 'Levels Added',
-          description: `Successfully added ${levelsToAdd} levels to ${targetUser.tag}.\n**New Level:** ${userData.level}`,
+          title: 'Уровни добавлены',
+          description:
+            `Пользователю ${targetUser.tag} успешно добавлено **${levelsToAdd}** ур. ` +
+            `\n**Новый уровень:** ${userData.level}`,
           color: 'success'
         })
       ]
