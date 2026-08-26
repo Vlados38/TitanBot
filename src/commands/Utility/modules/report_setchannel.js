30bef6380599e6a83734c20b3cfb869aaee25e1a
Vlados38 <54667505+Vlados38@users.noreply.github.com>
2026-08-26 18:31:45 +0300

    Update report_setchannel.js

--- src/commands/Utility/modules/report_setchannel.js
+++ src/commands/Utility/modules/report_setchannel.js
@@ -5,10 +5,14 @@ import { InteractionHelper } from '../../../utils/interactionHelper.js';
 import { logger } from '../../../utils/logger.js';
 
 import { replyUserError, ErrorTypes } from '../../../utils/errorHandler.js';
+
 export default {
     async execute(interaction, config, client) {
         if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
-            return await replyUserError(interaction, { type: ErrorTypes.PERMISSION, message: 'You need **Manage Server** permissions to set the report channel.' });
+            return await replyUserError(interaction, {
+                type: ErrorTypes.PERMISSION,
+                message: 'Вам нужны права **Управление сервером**, чтобы установить канал для жалоб.'
+            });
         }
 
         const channel = interaction.options.getChannel('channel');
@@ -19,14 +23,17 @@ export default {
 
             return InteractionHelper.safeReply(interaction, {
                 embeds: [successEmbed(
-                    'Report Channel Set',
-                    `All new reports will now be sent to ${channel}.\nYou can also manage this from \`/logging dashboard\`.`,
+                    'Канал для жалоб установлен',
+                    `Все новые жалобы теперь будут отправляться в ${channel}.\nВы также можете управлять этим через \`/logging dashboard\`.`,
                 )],
                 ephemeral: true,
             });
         } catch (error) {
-            logger.error('report_setchannel error:', error);
-            return await replyUserError(interaction, { type: ErrorTypes.UNKNOWN, message: 'Could not save the channel configuration.' });
+            logger.error('Ошибка report_setchannel:', error);
+            return await replyUserError(interaction, {
+                type: ErrorTypes.UNKNOWN,
+                message: 'Не удалось сохранить настройки канала.'
+            });
         }
     },
 };
