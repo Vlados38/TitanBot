8e0e81a4fdb8bb54cbba28f341c6ad0b9a098603
Vlados38 <54667505+Vlados38@users.noreply.github.com>
2026-08-26 18:31:59 +0300

    Update avatar.js

--- src/commands/Utility/avatar.js
+++ src/commands/Utility/avatar.js
@@ -3,33 +3,35 @@ import { createEmbed } from '../../utils/embeds.js';
 import { logger } from '../../utils/logger.js';
 
 import { InteractionHelper } from '../../utils/interactionHelper.js';
+
 export default {
     data: new SlashCommandBuilder()
-    .setName("avatar")
-    .setDescription("Display a user's avatar image")
-    .addUserOption((option) =>
-      option
-        .setName("target")
-        .setDescription(
-          "The user whose avatar you want to see (defaults to you)",
+        .setName("avatar")
+        .setDescription("Показать аватар пользователя")
+        .addUserOption((option) =>
+            option
+                .setName("target")
+                .setDescription(
+                    "Пользователь, чей аватар вы хотите посмотреть (по умолчанию — вы)",
+                ),
         ),
-    ),
 
-  async execute(interaction) {
-    const user = interaction.options.getUser("target") || interaction.user;
-    const avatarUrl = user.displayAvatarURL({ size: 2048, dynamic: true });
+    async execute(interaction) {
+        const user = interaction.options.getUser("target") || interaction.user;
+        const avatarUrl = user.displayAvatarURL({ size: 2048, dynamic: true });
 
-    const embed = createEmbed({ 
-      title: `${user.username}'s Avatar`, 
-      description: `[Download Link](${avatarUrl})` 
-    })
-      .setImage(avatarUrl);
+        const embed = createEmbed({
+            title: `Аватар пользователя ${user.username}`,
+            description: `[Скачать аватар](${avatarUrl})`
+        })
+            .setImage(avatarUrl);
 
-    await InteractionHelper.safeReply(interaction, { embeds: [embed] });
-    logger.info(`Avatar command executed`, {
-      userId: interaction.user.id,
-      targetUserId: user.id,
-      guildId: interaction.guildId
-    });
-  }
+        await InteractionHelper.safeReply(interaction, { embeds: [embed] });
+
+        logger.info(`Команда avatar выполнена`, {
+            userId: interaction.user.id,
+            targetUserId: user.id,
+            guildId: interaction.guildId
+        });
+    }
 };
