a57e0e8bee1f469a345ce5efd33fb386bc7c769b
Vlados38 <54667505+Vlados38@users.noreply.github.com>
2026-08-26 17:43:35 +0300

    Update shop.js

--- src/commands/Economy/shop.js
+++ src/commands/Economy/shop.js
@@ -3,9 +3,10 @@ import shopBrowse from './modules/shop_browse.js';
 
 export default {
     slashOnly: true,
+
     data: new SlashCommandBuilder()
         .setName('shop')
-        .setDescription('Browse the economy shop.'),
+        .setDescription('Просмотреть магазин экономики.'),
 
     async execute(interaction, config, client) {
         return shopBrowse.execute(interaction, config, client);
